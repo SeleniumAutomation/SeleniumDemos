2dcf695a876c9dba6598072ee16db9c7f6e0b33b
HMTech <HMTech@users.noreply.github.com>
2013-09-19 13:14:31 +0530

    FileUpload through AutoIt
    requisites: 
    AutoItX4Java.jar
    jacob-1.17-x64.dll
    jacob.jar
    and do register autoit dll (as per your version).

--- src/com/hm/demo/FileUpload.java
+++ src/com/hm/demo/FileUpload.java
@@ -2,30 +2,19 @@ package com.hm.demo;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import autoitx4java.AutoItX;
 
 public class FileUpload {
-
-	
 	public static void main(String[] args) {
-	
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.uploadify.com");
 		driver.manage().window().maximize();
-		
-		driver.findElement(By.xpath("html/body/div[2]/section[1]/div/div/nav/ul/li[3]/a")).click();
-		
-		
-		
-		driver.findElement(By.id("file_upload_1")).sendKeys("C:\\Users\\pratush.manglik\\Downloads\\manu.jpeg");
-		
-		
-		
-		
-		
-		
-		
+		driver.findElement(By.cssSelector("#menu-item-31>a")).click();
+		driver.findElement(By.cssSelector("#SWFUpload_0")).click();
+		AutoItX aux = new AutoItX();
+		aux.winWaitActive("Open");
+		aux.send("webData.csv");
+		aux.controlClick("Open", "&Open", "1");
 	}
-
 }
